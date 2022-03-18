3c3c04dec36d1345af93f7bf256b0af426f80786
ShayneC <1348656979@qq.com>
2022-03-18 21:19:14 +0800

    springboot整合elasticsearch并测试基本功能

---
+++ src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -0,0 +1,93 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class ElasticsearchTests {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private DiscussPostRepository discussPostRepository;
+
+    @Autowired
+    private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Test
+    public void testInsert() {
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(241));
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(242));
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(243));
+    }
+
+    @Test
+    public void testInsertList() {
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(101, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(102, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(103, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(111, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(112, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(131, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(132, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(133, 0, 100));
+        discussPostRepository.saveAll(discussPostMapper.selectDiscussPosts(134, 0, 100));
+    }
+
+    @Test
+    public void testUpdate() {
+        DiscussPost post = discussPostMapper.selectDiscussPostById(231);
+        post.setContent("我是新人，使劲灌水");
+        discussPostRepository.save(post);
+    }
+
+    @Test
+    public void testDelete() {
+        // discussPostRepository.deleteById(231);
+        discussPostRepository.deleteAll();
+    }
+
+    @Test
+    public void testSearchByRepository() {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .withSorts(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSorts(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSorts(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        // 底层获取得到了高亮显示的值，但是没有返回
+        SearchHits<DiscussPost> hits = elasticsearchRestTemplate.search(searchQuery, DiscussPost.class);
+        for (SearchHit<DiscussPost> hit : hits.getSearchHits()) {
+            DiscussPost post = hit.getContent();
+            for (String t : hit.getHighlightField("title")) {
+                post.setTitle(t);
+            }
+            for (String c : hit.getHighlightField("content")) {
+                post.setTitle(c);
+            }
+            System.out.println(post);
+        }
+    }
+}
